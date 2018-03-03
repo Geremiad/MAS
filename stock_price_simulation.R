85d3855f8c930514536e74c98dc0bbd7d2531100
Geremiad <huyigeremiad@gmail.com>
2018-03-02 23:38:25 -0800

    Update and rename future_stock_price_simulation.R to stock_price_simulation.R

--- stock_price_simulation.R
+++ stock_price_simulation.R
@@ -1,4 +1,4 @@
-# 1b
+# mean = 0.2, std = 0.25, delta t is 1 week, current stock price is 50
 u <- 0.2
 sigma <- 0.25
 s <- 50
@@ -7,7 +7,7 @@ t <- 1:52
 err <- rnorm(length(t))
 
 for (i in c(2:52)){
-  
+  # stochastic method
   s <- s*(1+u/length(t)+sigma*sqrt(1/length(t))*err[i-1])
   price[i] <- s
 }
